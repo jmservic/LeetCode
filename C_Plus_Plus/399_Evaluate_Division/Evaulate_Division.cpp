833c5a4142bb557138b4a201dc93c0b1d93c6b81
Sacriflces <Jmhservice@gmail.com>
2025-04-13 21:46:24 -0400

    Worked on Evaluate Division in Cpp, need to figure out the issue with my struct when it is being inserted into the unordered_map

--- C_Plus_Plus/399_Evaluate_Division/Evaulate_Division.cpp
+++ C_Plus_Plus/399_Evaluate_Division/Evaulate_Division.cpp
@@ -10,6 +10,12 @@ class Solution {
     {
         double multiplier;
         string variable;
+        mult_var(double multiplier, string variable) : multiplier(multiplier), variable(variable) {}
+        mult_var(mult_var& obj)
+        {
+            multiplier = obj.multiplier;
+            variable = obj.variable;
+        }
     } mult_var;
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
@@ -27,23 +33,45 @@ public:
         return results;
     }
 
-    void defineEqualities(unordered_map<string, mult_var> &varMap, vector<vector<string>>& equations, vector<double>& values)
+    void defineEqualities(unordered_map<string, mult_var> &varMap, const vector<vector<string>>& equations, const vector<double>& values)
     {
         //Loop through the equations
-        //If both operands are defined, continue
-        //If both operands are not defined, define the second as itself, and continue
-        //to the case where only the first 1 isn't define.
-        //If only the second isn't defined, define it in terms of the first.
-
+        for(int i = 0; i < equations.size(); ++i)
+        {
+            string op1 = equations[i][0], op2 = equations[i][0];
+            bool containsOp1 = varMap.contains(op1), containsOp2 = varMap.contains(op2);
+            //If both operands are defined, continue
+            if(containsOp1 && containsOp2) continue;
+            //If both operands are not defined, define the second as itself, and continue
+            //to the case where only the first 1 isn't define.
+            if(!containsOp1 && !containsOp2)
+            {
+                varMap.emplace(op2, mult_var(1, op2));
+                containsOp2 = true;
+            }
+            //If only the first isn't defined, define it in terms of the second
+            if(!containsOp1 && containsOp2)
+                varMap.emplace(op1, mult_var(values[i], op2));                
+            else if (containsOp1 && !containsOp2) //If only the second isn't defined, define it in terms of the first.
+                varMap.emplace(op2, mult_var(1 / values[i], op1));
+        }   
     }
 
     double calcQuery(const unordered_map<string, mult_var> &varMap, const vector<string> &query)
     {
-
+        if (!validQuery(varMap, query))
+            return -1.0;
+        
+        string op1 = query[0], op2 = query[1];
+    
+        double result = varMap.at(op1).multiplier / varMap.at(op2).multiplier;
+        return result;
     }
 
     bool validQuery(const unordered_map<string, mult_var> &varMap, const vector<string> &query) 
     {
-
+        string op1 = query[0], op2 = query[1];
+        
+        return varMap.at(op1).variable == varMap.at(op2).variable;
     }
 };
